90324ee53e6aa2a41bfa93bfc466a1f2e4357402
屿东 <2395183536@qq.com>
2023-07-30 20:29:31 +0800

    🎉 init: 项目初始化

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src') // src 路径
+		}
+	},
+	server: {
+		port: 8080, // 开发环境启动的端口
+		proxy: {
+			'/api': {
+				// 当遇到 /api 路径时，将其转换成 target 的值
+				target: 'http://xx.xx.xx.xx:8080/api',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, '') // 将 /api 重写为空
+			}
+		}
+	}
+})
