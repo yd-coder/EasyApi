83fc8bfbb68226e81df73306c096e784c3a83b70
TOBYhhw <1505994122@qq.com>
2023-07-31 08:41:09 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useRoutes } from 'react-router-dom'
 import routes from './routes'
+import { log } from 'console'
 
 const App: React.FC = () => {
 	return useRoutes(routes)
 }
+log('App.tsx')
 
 export default App
